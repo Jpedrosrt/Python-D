70a0536caf9c1eaf2a7309e0c32014e6f8d72c97
João Pedro <j0310pedro@gmail.com>
2021-11-18 20:01:35 -0300

    Update DESAFIO 103.py

--- D/DESAFIO 103.py
+++ D/DESAFIO 103.py
@@ -1,19 +1,15 @@
-def ficha(nome, gols):
-    cont = 0
-    for a in gols:
-        if a in '0123456789':
-            cont += 1
-    if cont == len(gols):
-        gols = int(gols)
-    if len(nome) == 0:
-        nome = "<desconhecido>"
-    if type(gols) == str:
-        if len(gols) == 0 or type(gols) == str:
-            gols = 0
-    return f'O jogador {nome} fez {gols} gols no campeonato.'
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato.')
+
 
 
 n = str(input('Nome do jogador: ')).strip()
 g = str(input('Número de gols: ')).strip()
-print(ficha(n, g))
-
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
