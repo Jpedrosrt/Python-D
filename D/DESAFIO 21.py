553f95eda7e272a989d45b4e1f2f3f1ea5210652
João Pedro <93775940+Jpedrosrt@users.noreply.github.com>
2021-11-28 15:57:06 -0300

    Update DESAFIO 21.py

--- D/DESAFIO 21.py
+++ D/DESAFIO 21.py
@@ -4,3 +4,4 @@ pygame.init()
 pygame.mixer.music.load('DOKI.mp3')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy(): pass
+# Link da musica: "https://www.youtube.com/watch?v=h5v5jUtlgf8"
